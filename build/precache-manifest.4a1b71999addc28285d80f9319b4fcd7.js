89400fe7a8fde85694cd817bbb3ef734f5aea94c
liqiang <a15515358762@qq.com>
2020-03-30 19:00:48 +0800

    简易

---
+++ build/precache-manifest.4a1b71999addc28285d80f9319b4fcd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4caa28165130cb4026610ffa9a6b74ce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6cd7fd51e722a258443",
+    "url": "./static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "b6cd7fd51e722a258443",
+    "url": "./static/js/2.eb33a376.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "./static/js/2.eb33a376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87053f48f0121f9b6f2f",
+    "url": "./static/js/main.78fd35da.chunk.js"
+  },
+  {
+    "revision": "6983486f1a659f6f9771",
+    "url": "./static/js/runtime-main.87aa16b9.js"
+  }
+]);
